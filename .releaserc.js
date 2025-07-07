b71b3723878420bc3ea7fd7051adfe5d93847f0b
obergerkatz <Omer.BergerKatz@vonage.com>
2025-07-07 12:24:58 +0300

    fix(index): remove packages not used in package.json

--- .releaserc.js
+++ .releaserc.js
@@ -6,12 +6,6 @@ module.exports = {
     "plugins": [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        [
-            "@semantic-release/npm",
-            {
-                "npmPublish": false
-            }
-        ],
         "@semantic-release/github"
     ]
 };
